f18c136f5f35827754a2c9c4ba58eebbe7ba9a9c
Mdakhib <mohamedakhib@gmail.com>
2021-06-24 15:22:56 +0530

    temparature and sunset made dynamic

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -103,24 +103,34 @@ const WeatherInfoComponent = (props) => {
 };
 
 function WeatherComponent(props) {
-  const { weather } = props;
+    const { weather } = props;
+    const isDay = weather?.weather[0].icon?.includes("d");
+    const getTime = (timeStamp) => {
+      return `${new Date(timeStamp * 1000).getHours()} : ${new Date(
+        timeStamp * 1000
+      ).getMinutes()}`;
+    };
 
   return (
     <>
       <WeatherCondition>
         <Condition>
           {" "}
-          <span>30 C</span> | Cloudy{" "}
+          <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span> |{" "}
+          {`${weather?.weather[0].description}`}
         </Condition>
         <WeatherLogo src="/icons/perfect-day.svg" />
       </WeatherCondition>
       <Location>{`${weather?.name}, ${weather?.sys?.country}`} </Location>
       <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
       <WeatherInfoContainer>
-        <WeatherInfoComponent name="sunrise" value="" />
+        <WeatherInfoComponent
+          name={isDay ? "sunset" : "sunrise"}
+          value={getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}
+        />
         <WeatherInfoComponent name="humidity" value={weather?.main?.humidity} />
         <WeatherInfoComponent name="wind" value={weather?.wind?.speed} />
-              <WeatherInfoComponent name="pressure" value={weather?.main?.pressure} />
+        <WeatherInfoComponent name="pressure" value={weather?.main?.pressure} />
       </WeatherInfoContainer>
     </>
   );
